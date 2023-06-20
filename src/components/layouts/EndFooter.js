457d26015d81229df0fc298fcf9fde23e9e7bd99
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-06-20 20:32:09 +0430

    add logo to footer

--- src/components/layouts/EndFooter.js
+++ src/components/layouts/EndFooter.js
@@ -1,10 +1,22 @@
+import { FaFacebook, FaLinkedin, FaInstagram } from 'react-icons/fa';
 function CopyRight() {
   const currentYear = new Date().getFullYear().toString();
   return (
-    <div className=' bg-mainNavColor h-36 w-full'>
+    <div className=' bg-mainNavColor h-36 w-full flex flex-col items-center justify-center'>
       <p className='mt-4 max-w-6xl text-center mx-auto'>
         &copy; copyright {currentYear} - INFYA{' '}
       </p>
+      <div className='flex items-center justify-center gap-6 mt-10 text-2xl'>
+        <a href='/' target='_blank'>
+          <FaFacebook />
+        </a>
+        <a href='/' target='_blank'>
+          <FaLinkedin />
+        </a>
+        <a href='/' target='_blank'>
+          <FaInstagram />
+        </a>
+      </div>
     </div>
   );
 }
