e2914c1d64a30d924a7af992fbff1a0b67938aea
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-06-22 22:15:26 +0430

    fix tags

--- src/components/modules/projects/ProjectsCats.js
+++ src/components/modules/projects/ProjectsCats.js
@@ -5,42 +5,64 @@ import catDataDB from '../../../../data/catDataDB.json';
 function ProjectsCats() {
   const [selectCat, setSelectCat] = useState('');
 
-  const handleCat = (id) => setSelectCat(id);
-  const { prCats } = catDataDB;
-  const cat = new Set();
+  const handleCat = (e) => setSelectCat(e.target.innerHTML);
 
   return (
     <article className='w-full h-full  flex flex-col items-center justify-center '>
       {/* select cats */}
       <section className='flex items-center justify-between mt-10 flex-wrap w-3/4 max-w-6xl px-4'>
         <p
-          onClick={() => setSelectCat('')}
+          onClick={(e) => handleCat(e)}
           className={`${
-            selectCat === '' && 'bg-slate-600'
-          } text-mainTxtColor px-3 py-1 hover:bg-slate-600 cursor-pointer uppercase`}
+            selectCat === '' || (selectCat === 'all projects' && 'bg-backFilter')
+          } text-mainTxtColor px-3 py-1 my-2 transition duration-500 hover:bg-backFilter cursor-pointer uppercase`}
         >
           all projects
         </p>
-        {prCats.map((item) => {
-          // removing duplicated cats
-          cat.add(item.catName);
-          console.log([cat]);
-          return (
-            <p
-              onClick={() => handleCat(item.catName)}
-              className={`${
-                selectCat === item.catName ? 'bg-slate-600' : ''
-              } hover:bg-slate-600 px-3 py-1 transition duration-500 cursor-pointer text-mainTxtColor uppercase`}
-              key={item.id}
-            >
-              {item.catName}
-            </p>
-          );
-        })}
+        <p
+          onClick={(e) => handleCat(e)}
+          className={`${
+            selectCat === 'RETAIL' && 'bg-backFilter'
+          } text-mainTxtColor px-3 py-1 my-2 transition duration-500 hover:bg-backFilter cursor-pointer uppercase`}
+        >
+          RETAIL
+        </p>
+        <p
+          onClick={(e) => handleCat(e)}
+          className={`${
+            selectCat === 'CONSULTANCY' && 'bg-backFilter'
+          } text-mainTxtColor px-3 py-1 my-2 transition duration-500 hover:bg-backFilter cursor-pointer uppercase`}
+        >
+          CONSULTANCY
+        </p>
+        <p
+          onClick={(e) => handleCat(e)}
+          className={`${
+            selectCat === 'CORPORATE' && 'bg-backFilter'
+          } text-mainTxtColor px-3 py-1 my-2 transition duration-500 hover:bg-backFilter cursor-pointer uppercase`}
+        >
+          CORPORATE
+        </p>
+        <p
+          onClick={(e) => handleCat(e)}
+          className={`${
+            selectCat === 'HOSPITALITY' && 'bg-backFilter'
+          } text-mainTxtColor px-3 py-1 my-2 transition duration-500 hover:bg-backFilter cursor-pointer uppercase`}
+        >
+          HOSPITALITY
+        </p>
+        <p
+          onClick={(e) => handleCat(e)}
+          className={`${
+            selectCat === 'LANDSCAPING' && 'bg-backFilter'
+          } text-mainTxtColor px-3 py-1 my-2 transition duration-500 hover:bg-backFilter cursor-pointer uppercase`}
+        >
+          LANDSCAPING
+        </p>
 
         {/* select cats */}
       </section>
-
+      {/* /////////////////////////////////////// */}
       {/* render cats */}
       <section className='mt-24 h-full w-full mx-auto'>
         <RenderCats
