a7795a723c9be67993f5868124419dbe77e872f2
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-06-27 00:59:29 +0430

    update homepage about section

--- src/components/templates/HomePage.js
+++ src/components/templates/HomePage.js
@@ -14,51 +14,71 @@ function HomePage() {
         {/* video */}
 
         {/* about us */}
-        <div className='h-fit  w-full grid grid-cols-1 lg:grid-cols-2  lg:content-center  gap-4'>
-          <div className='w-full p-0 m-0 mx-auto h-1/2 lg:h-full lg:w-[100%] self-end lg:self-center'>
-            <div className='block w-full h-full '>
-              <Image
-                className='block w-full h-full lg:h-3/4 object-cover'
-                src='/home-page.jpg'
-                width={500}
-                height={500}
-                alt='about us '
-              />
-            </div>
-          </div>
-          <div className='pb-10 px-4 lg:pb-0 lg:h-full'>
-            <div className='text-mainTxtColor py-10 px-4 text-lg '>
-              <h1 className='text-4xl capitalize mb-3 lg:h-full'>about us</h1>
-              <p className='lg:h-full'>
-                We provide professional Engineering services with, architectural
-                & interior design, Home automation, and maintenance services
-                exclusively to the luxury retail sector and affiliated business
-                premises.
-              </p>
-              <div className='py-4'>
-                <h1 className='text-4xl capitalize pb-2'>Our Vision</h1>
-                <p className=' lg:h-full'>
-                  At Master Engineering we have the vision to bring
-                  architectural & interior design, Home Automation, and
-                  mechanical & electrical installation, together to create a
-                  modern, fully integrated environment. With the advancement in
-                  smartphones and technology, has allowed us to use this
-                  technology to create an environment where Luxury designs merge
-                  with electrical equipment is controlled by a single smart
-                  device. There is continued research and development to create
-                  energy-efficient smart automation and we keep fully up to date
-                  on the latest technology to offer our clients.
-                </p>
+        {/* <div className='h-fit  w-full grid grid-cols-1 lg:grid-cols-2  lg:content-center  gap-4'> */}
+        <section className='lg:min-h-screen lg:flex lg:flex-col  '>
+          {/* about */}
+          <article className='w-full h-full  lg:flex  '>
+            <div className='w-full p-0 m-0 mx-auto h-1/2 lg:h-full lg:w-[100%] self-end lg:self-center'>
+              <div className='block w-full h-1/2 lg:h-full'>
+                <Image
+                  className='block w-full h-full object-cover'
+                  src='/home-page.jpg'
+                  width={500}
+                  height={500}
+                  alt='about us '
+                />
               </div>
-              <Link
-                href='/about'
-                className='self-start mt-10 block w-fit hover:bg-mainNavColor hover:border-mainNavColor transition duration-700 bg-inherit text-sm border px-7 py-1 my-5 uppercase text-mainTxtColor font-light tracking-wider'
-              >
-                read more
-              </Link>
             </div>
-          </div>
-        </div>
+            <div className='pb-10 px-4 lg:pb-0 lg:h-full lg:w-3/4'>
+              <div className='text-mainTxtColor py-10 px-4 text-lg '>
+                <h1 className='text-4xl capitalize mb-3 lg:h-full'>about us</h1>
+                <p className='lg:h-full'>
+                  We provide professional Engineering services with,
+                  architectural & interior design, Home automation, and
+                  maintenance services exclusively to the luxury retail sector
+                  and affiliated business premises.
+                </p>
+
+                <Link
+                  href='/about'
+                  className='self-start mt-10 block w-fit hover:bg-mainNavColor hover:border-mainNavColor transition duration-700 bg-inherit text-sm border px-7 py-1 my-5 uppercase text-mainTxtColor font-light tracking-wider'
+                >
+                  read more
+                </Link>
+              </div>
+            </div>
+          </article>
+          {/* our vision */}
+          <article className='w-full h-full lg:flex lg:flex-row-reverse mt-10'>
+            <div className='w-full p-0 m-0 mx-auto h-1/2 lg:h-full lg:w-[100%] self-end lg:self-center'>
+              <div className='block w-full h-1/2 lg:h-full'>
+                <Image
+                  className='block w-full h-full object-cover'
+                  src='/home-page.jpg'
+                  width={500}
+                  height={500}
+                  alt='about us '
+                />
+              </div>
+            </div>
+            <div className='p-4 text-mainTxtColor lg:w-3/4'>
+              <h1 className='text-4xl capitalize pb-2'>Our Vision</h1>
+              <p className=' lg:h-full lg:w-full'>
+                At Master Engineering we have the vision to bring architectural
+                & interior design, Home Automation, and mechanical & electrical
+                installation, together to create a modern, fully integrated
+                environment. With the advancement in smartphones and technology,
+                has allowed us to use this technology to create an environment
+                where Luxury designs merge with electrical equipment is
+                controlled by a single smart device. There is continued research
+                and development to create energy-efficient smart automation and
+                we keep fully up to date on the latest technology to offer our
+                clients.
+              </p>
+            </div>
+          </article>
+        </section>
+        {/* </div> */}
         {/* about us */}
 
         {/* OUR DESIGN & BUILD SERVICES */}
